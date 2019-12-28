1b7b6fd549b4a9d12c303d338cf06ec3f1484bf2
Peter Centellini <peter.c@trivec.se>
2019-12-28 16:19:14 +0100

    Added Programming folder and documents

---
+++ Programming/DominoQueries.sql
@@ -0,0 +1,9 @@
+select * from txpaymentline where ticketkey = 3316200083823830
+
+update txpaymentline set dmxhold = 1 where dmxkey = 3320602425294849 and TxOrder_DmxKey = 3317303890454556
+
+select * from txorderline where ticketkey = 3316200083823830
+
+select * from txticket where dmxkey = 3316204378797009
+
+select * from TxOrderLine where ticketkey = 3316204378797009
